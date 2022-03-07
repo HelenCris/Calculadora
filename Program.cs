37b779e620e31d978f6405b46e3669dad3c2cf05
HelenCris <79730539+HelenCris@users.noreply.github.com>
2022-03-07 17:36:30 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int age = 14;
+
+            Console.WriteLine($"There once was a man named {name}");
+            Console.WriteLine($"He was {age} years old");
+            Console.WriteLine($"He really liked the name {name}");
+            Console.WriteLine($"But didn't like being {age}");
+
+            Console.ReadLine();
+        }
+    }
+}
