ef49ea72f558a66e73591469566df7a0dacd72bb
HelenCris <79730539+HelenCris@users.noreply.github.com>
2022-03-08 08:48:32 -0300

    projeto finalizado

--- Program.cs
+++ Program.cs
@@ -11,15 +11,48 @@ namespace calculadora
     {
         static void Main(string[] args)
         {
-            string name = "John";
-            int age = 14;
+            //input do primeiro número
+            Console.WriteLine("Escreva um número: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            //input do segundo número
+            Console.WriteLine("Escreva um segundo número: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            //escolha da operação
+            Console.WriteLine("MENU:");
+            Console.WriteLine("+ para soma");
+            Console.WriteLine("- para subtração");
+            Console.WriteLine("* para multiplicação");
+            Console.WriteLine("% para divisão");
+            Console.WriteLine("Escolha o tipo de operação que você gostaria de realizar: ");
+            string operacao = Console.ReadLine();
 
-            Console.WriteLine($"There once was a man named {name}");
-            Console.WriteLine($"He was {age} years old");
-            Console.WriteLine($"He really liked the name {name}");
-            Console.WriteLine($"But didn't like being {age}");
+            //resultado das operações
+            int soma = num1 + num2;
+            int subtracao = num1 - num2;
+            int multiplicacao = num1 * num2;
+            int divisao = num1 / num2;
 
-            Console.ReadLine();
+
+            if (operacao == "+") 
+            { 
+                Console.WriteLine($"O resultado da soma é {soma}");
+            }
+            else if (operacao == "-") 
+            { 
+                Console.WriteLine($"O resultado da subtração é {subtracao}");
+            }
+            else if (operacao == "*") 
+            { 
+                Console.WriteLine($"O resultado da multiplicação é {multiplicacao}");
+            }
+            else if (operacao == "/") 
+            { 
+                Console.WriteLine($"O resultado da divisão é {divisao}");
+            }
+            else 
+            {
+                Console.WriteLine("Operador inválido! Tente Novamente");
+            }
         }
     }
 }
